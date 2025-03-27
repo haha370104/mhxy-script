d5e224c6443b82910ff5bf1aa363a775cc7a3478
tomato <137368440@qq.com>
2025-03-27 21:59:33 +0800

    feat: 增加强化前检查，避免宝石在第二格，导致宝石全部消失

--- helper/equipment.py
+++ helper/equipment.py
@@ -1,7 +1,8 @@
-from helper.image import find_img_position
+from helper.image import find_img_position, template_matching, get_image
 from helper.mouse import find_img_click
 from ascript.windows.action import mouse_move, click, key_down, key_up, input
 from time import sleep
+from ascript.windows.screen import capture
 
 
 def move_inventory_position(base_coordinate, item_order):
@@ -31,24 +32,43 @@ def split_stone(stone_position, times):
         print(f'完成第{i}次拆分')
 
 
-def strengthen_equip(equip_position, stone_range):
-    [basic_x, basic_y] = find_img_position('picture\\strengthen_title.png')
+def strength_pre_check(x, y):
+    x_offset, y_offset = -193, 98
+    basic_x, basic_y = x + x_offset, y + y_offset
+
+    first_plot = capture(rect=[basic_x, basic_y, basic_x + 50, basic_y + 50])
+    stone = get_image('picture\\strengthen_stone.png')
+    match_result = template_matching(first_plot, stone)[0].size > 0
+    return match_result
+
+
+def strengthen_equip(equip_position, stone_position, basic_coordinate):
+    basic_x, basic_y = basic_coordinate
+
     x_offset, y_offset = -8, 108
     confirm_x_offset, confirm_y_offset = -158, 291
 
     base_coordinate = [basic_x + x_offset, basic_y + y_offset]
-    for i in stone_range:
-        move_inventory_position(base_coordinate, i)
-        click(button='left')
-        sleep(0.3)
-        move_inventory_position(base_coordinate, equip_position)
-        click(button='left')
-        sleep(0.3)
-        click(basic_x + confirm_x_offset, basic_y + confirm_y_offset, button='left')
-        sleep(0.3)
+    move_inventory_position(base_coordinate, stone_position)
+    click(button='left')
+    sleep(0.3)
+    move_inventory_position(base_coordinate, equip_position)
+    click(button='left')
+    sleep(0.3)
+    check_result = strength_pre_check(basic_x, basic_y)
+    if not check_result:
+        click(x=basic_x - 163, y=basic_y + 130)
+        sleep(0.2)
+        click(x=basic_x - 78, y=basic_y + 130)
+        return strengthen_equip(equip_position, stone_position, basic_coordinate)
+
+    click(basic_x + confirm_x_offset, basic_y + confirm_y_offset, button='left')
+    sleep(0.3)
 
 
 def strength(equip_count=4, stone_per_equip=15):
+    basic_x, basic_y = find_img_position('picture\\strengthen_title.png')
+
     equip_range = list(range(0, equip_count))
     stone_position = equip_count
     print('请保证装备从第一格开始连续放置，随后一格放强化石，程序于3秒后运行')
@@ -56,5 +76,5 @@ def strength(equip_count=4, stone_per_equip=15):
 
     for equip in equip_range:
         for i in range(stone_per_equip):
-            strengthen_equip(equip, range(stone_position, stone_position + 1))
+            strengthen_equip(equip, stone_position, [basic_x, basic_y])
             print(f'对第{equip}件装备完成第{i}次镶嵌')
--- helper/image.py
+++ helper/image.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from ascript.windows.screen import find_images, capture
 from ascript.windows import window
 import os
@@ -18,12 +19,28 @@ def get_game_screen():
     screen = capture(game_handler)
     return screen
 
+def get_image(image_name: str):
+    image_path = os.path.join(os.path.dirname(__file__), '..\\', image_name)
+    return Image.open(image_path)
 
 def find_img_position(img_name: str):
     image_path = os.path.join(os.path.dirname(__file__), '..\\', img_name)
-    result = find_images(image_path, confidence=0.5, res_num=0)
+    result = find_images(image_path, confidence=0.8, res_num=0)
     first_match_point = result[0]['result']
     return first_match_point
+
+def template_matching(whole_image: Image, template_image: Image):
+    cv2_whole_image = transform_PIL_to_cv2(whole_image)
+    cv2_template_image = transform_PIL_to_cv2(template_image)
+
+    h, w, _ = cv2_template_image.shape
+    method = cv2.TM_CCOEFF_NORMED
+    res = cv2.matchTemplate(cv2_whole_image, cv2_template_image, method)
+
+    loc = np.where(res >= 0.5)
+
+    return loc
+
 
 def transform_PIL_to_cv2(img):
     # 将PIL图像转换为NumPy数组
